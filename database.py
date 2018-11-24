d53c796551f4f6e23fb2c0943430a4865ac19f29
unknown <torftorf@163.com>
2018-11-24 09:38:31 +0800

    First commit

---
+++ database.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  6 14:37:25 2018
+
+@author: Ed
+"""
+
+from sqlalchemy import Column,String,create_engine
+from sqlalchemy.types import CHAR,Integer,String,Text,Time,Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+# from flask import jsonify
+from sqlalchemy import func
+
+
+Base = declarative_base()
+
+class Region(Base):
+    __tablename__ = 'region'    
+    region= Column('region',String(10),primary_key=True,nullable=False)
+    ver=Column('ver',Integer)
+    port = Column('port',String(3))
+    remark=Column('remark',Text)
+
+class Account(Base):
+    __tablename__='account'
+    id= Column('id',String(10),primary_key=True,nullable=False)
+    function=Column('function',String(10))
+
+class Arbitry(Base):
+    __tablename__='arbitry'
+    dest=Column('dest',String(50))
+    orig=Column('orig',String(50),primary_key=True,nullable=False)
+    a_40=Column('a_40',Integer)
+    a_20=Column('a_20',Integer)
+    a_hq=Column('a_hq',Integer)
+    valid=Column('valid',Date)
+
+class Freight(Base):
+    __tablename__='freight'
+    terms=Column('terms',Text)
+    ver=Column('ver',Integer,primary_key=True,nullable=False)
+    region= Column('region',String(10))
+    port = Column('port',String(3))
+    dest=Column('dest',String(50))
+    fak_20=Column('fak_20',Integer)
+    fak_40=Column('fak_40',Integer)
+    fak_hq=Column('fak_hq',Integer)
+    br_20=Column('br_20',Integer)    
+    br_40=Column('br_40',Integer)
+    br_hq=Column('br_hq',Integer)
+    dir_trans=Column('dir_trans',String(1))
+
+class Contact(Base):
+    __tablename__='contact_list'
+    region= Column('region',String(10),primary_key=True,nullable=False)
+    addressee=Column('addressee', String(50))
+    email=Column('email',String(50))
+    to_or_copy=Column('to_or_copy',String(1))
+
+
+class Ver(Base):
+    __tablename__='ver'
+    created=Column('created',Time)
+    ver=Column('ver',Integer,primary_key=True,nullable=False)
+    published=Column('published',Time)
+    creator=Column('creator',String(10))
+
+engine = create_engine('postgresql://postgres:postgres@localhost:5432/workflow'
+                       ,echo=False)
+DBsession = sessionmaker(bind=engine)
+session = DBsession()
+
+
---
+++ ui/logon1.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'logon1.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+
+class Ui_fmLogon(object):
+    def setupUi(self, fmLogon):
+        fmLogon.setObjectName("fmLogon")
+        fmLogon.resize(400, 300)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(fmLogon.sizePolicy().hasHeightForWidth())
+        fmLogon.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(12)
+        fmLogon.setFont(font)
+        fmLogon.setMouseTracking(True)
+        fmLogon.setTabletTracking(True)
+        fmLogon.setFocusPolicy(QtCore.Qt.TabFocus)
+        fmLogon.setInputMethodHints(QtCore.Qt.ImhHiddenText|QtCore.Qt.ImhSensitiveData)
+        self.label = QtWidgets.QLabel(fmLogon)
+        self.label.setGeometry(QtCore.QRect(60, 80, 81, 21))
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(12)
+        self.label.setFont(font)
+        self.label.setToolTipDuration(-1)
+        self.label.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(fmLogon)
+        self.label_2.setGeometry(QtCore.QRect(60, 140, 81, 21))
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(12)
+        self.label_2.setFont(font)
+        self.label_2.setToolTipDuration(-1)
+        self.label_2.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
+        self.label_2.setObjectName("label_2")
+        self.groupBox = QtWidgets.QGroupBox(fmLogon)
+        self.groupBox.setGeometry(QtCore.QRect(50, 189, 301, 51))
+        self.groupBox.setTitle("")
+        self.groupBox.setObjectName("groupBox")
+        self.btnYes = QtWidgets.QPushButton(self.groupBox)
+        self.btnYes.setGeometry(QtCore.QRect(10, 10, 121, 31))
+        self.btnYes.setObjectName("btnYes")
+        self.btnChangePassword = QtWidgets.QPushButton(self.groupBox)
+        self.btnChangePassword.setGeometry(QtCore.QRect(170, 10, 121, 31))
+        self.btnChangePassword.setObjectName("btnChangePassword")
+        self.tbUser = QtWidgets.QLineEdit(fmLogon)
+        self.tbUser.setGeometry(QtCore.QRect(160, 80, 181, 20))
+        self.tbUser.setInputMethodHints(QtCore.Qt.ImhNone)
+        self.tbUser.setObjectName("tbUser")
+        self.tbPassword = QtWidgets.QLineEdit(fmLogon)
+        self.tbPassword.setGeometry(QtCore.QRect(160, 140, 181, 20))
+        self.tbPassword.setInputMethodHints(QtCore.Qt.ImhNone)
+        self.tbPassword.setObjectName("tbPassword")
+        self.groupBox.raise_()
+        self.label.raise_()
+        self.label_2.raise_()
+        self.tbUser.raise_()
+        self.tbPassword.raise_()
+
+        self.retranslateUi(fmLogon)
+        self.btnYes.clicked.connect(self.Log_confirm)
+        QtCore.QMetaObject.connectSlotsByName(fmLogon)
+
+    def retranslateUi(self, fmLogon):
+        _translate = QtCore.QCoreApplication.translate
+        fmLogon.setWindowTitle(_translate("fmLogon", "登录"))
+        self.label.setText(_translate("fmLogon", "用户名："))
+        self.label_2.setText(_translate("fmLogon", "密码："))
+        self.btnYes.setText(_translate("fmLogon", "确认"))
+        self.btnChangePassword.setText(_translate("fmLogon", "更改密码"))
+        self.tbPassword.setInputMask(_translate("fmLogon", "*"))
+
+    def Log_confirm(self):
+        QtWidgets.QMessageBox.information(QtWidgets,"标题","这是第一个PyQt5 GUI程序")
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    fmLogon = Ui_fmLogon()
+    fmLogon.setupUi(MainWindow)
+    MainWindow.show()
+    
+sys.exit(app.exec_())
